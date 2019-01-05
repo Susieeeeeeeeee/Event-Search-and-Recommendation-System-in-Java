ade628e51d517fc8d4d1ae701d4a084be8511c29
Susieeeeeeeeee <jinnianshu@hotmail.com>
2019-01-04 20:41:13 -0600

    algorithm for recommendation

--- entity/Item.java
+++ entity/Item.java
@@ -65,6 +65,30 @@ public class Item {
 		return obj;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (itemId == null) {
+			if (other.itemId != null)
+				return false;
+		} else if (!itemId.equals(other.itemId))
+			return false;
+		return true;
+	}
+
 	public static class ItemBuilder { // must be static, 如果不是static, 必须现先item类，但是
 		private String itemId; 		  // 我就是要item类
 		private String name; 
